90f82c16ae4554345e19f1fb420aa86121adf621
Raul R. Soares <83381403+raulrsoares@users.noreply.github.com>
2022-08-06 18:01:10 -0300

    06/08/2022 - SeteSegmentosBotao
    TERMINAR COM URGENCIA

---
+++ SA3_SeteSegmentosBotao/SA3_SeteSegmentosBotao.ino
@@ -0,0 +1,22 @@
+int timer = 350;
+int pino;
+int cnt = 0;
+void setup() {
+  for (pino = 0; pino < 4; pino ++) {
+    pinMode(pino, OUTPUT);
+  }
+  pinMode(7, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(7) == 1) {
+    cnt++;
+    PORTD = cnt;
+    delay(timer);
+  }
+
+  if (cnt <= 9) {
+    
+  }
+}
