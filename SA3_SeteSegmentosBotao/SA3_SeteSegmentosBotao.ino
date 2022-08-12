2af438d2fb65d8423eedb2885e9fe297f13c3564
Raul R. Soares <83381403+raulrsoares@users.noreply.github.com>
2022-08-11 21:13:29 -0300

    Update SA3_SeteSegmentosBotao.ino

--- SA3_SeteSegmentosBotao/SA3_SeteSegmentosBotao.ino
+++ SA3_SeteSegmentosBotao/SA3_SeteSegmentosBotao.ino
@@ -11,12 +11,49 @@ void setup() {
 
 void loop()
 {
-  if (digitalRead(7) == 1) {
+    /*if (digitalRead(7) == 1) {
     cnt++;
     PORTD = cnt;
     delay(timer);
-	if (cnt >= 9) { //Se contador for maior ou igual a 9
-    cnt = -1; //Atribui 0 a contador
+	if (cnt >= 9) {//Se contador for maior ou igual a 9
+     cnt = -1;//Atribui 0 a contador
   	}
-  }
+    }*/
+
+// ============Vai de 9 direto para 0============
+    if (digitalRead(7) == 1) {
+        VaiVolta();
+    }
+}
+// ============Adiciona 1 na cnt============
+void Incrementa() {
+    cnt++;//Incrementa contador em 1
+    PORTD = cnt;
+    delay(timer);
+}
+
+// ============Tira 1 na cnt============
+void Decrementa(){
+    cnt--;//Decrementa contador em 1
+    PORTD = cnt;
+    delay(timer);
+    
+}
+
+// ============Adiciona 1 até 9, chegando lá manda para 0============
+void Adiciociona (){
+    Incrementa();
+    if (cnt >= 9){
+    cnt = -1;//Atribui 0 a contador 
+    }
+}  
+
+// ============Vai de 9 a 0 e de 0 a 9============
+void VaiVolta(){
+    if (cnt <= 9){
+    Incrementa();
+    }
+    else {
+        Decrementa();
+    }
 }
