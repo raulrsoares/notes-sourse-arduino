1bab4109c4d84123a4cbf894ba4bb269245beebc
Raul R. Soares <83381403+raulrsoares@users.noreply.github.com>
2022-08-13 09:05:55 -0300

    indentação

--- SA3_SeteSegmentosBotao/SA3_SeteSegmentoBotao-2.cpp
+++ SA3_SeteSegmentosBotao/SA3_SeteSegmentoBotao-2.cpp
@@ -2,35 +2,35 @@ int timer = 500;
 int pino;
 int cnt = 0;
 void setup() {
-for (pino = 0; pino < 4; pino ++) {
+  for (pino = 0; pino < 4; pino ++) {
     pinMode(pino, OUTPUT);
-}
-pinMode(7, INPUT);
+  }
+  pinMode(7, INPUT);
 }
 void loop()
 {
-    if (digitalRead(7) == 1) {
-    if (cnt >= -1){
-            if (digitalRead(7) == 1) {
-            cnt++;//Incrementa contador em 1
-            PORTD = cnt;
-            delay(timer);
-            }
-        }
-        if (cnt >= 9){
-            if (digitalRead(7) == 1) {
-            cnt--;//Decrementa contador em 1
-            PORTD = cnt;
-            delay(timer);
-        }
-            while (cnt != 0)
-            {
-                if (digitalRead(7) == 1) {
-                cnt--;//Decrementa contador em 1
-                PORTD = cnt;
-                delay(timer);
-                }
-            }
-        }
+  if (digitalRead(7) == 1) {
+    if (cnt >= -1) {
+      if (digitalRead(7) == 1) {
+        cnt++;//Incrementa contador em 1
+        PORTD = cnt;
+        delay(timer);
+      }
     }
+    if (cnt >= 9) {
+      if (digitalRead(7) == 1) {
+        cnt--;//Decrementa contador em 1
+        PORTD = cnt;
+        delay(timer);
+      }
+      while (cnt != 0)
+      {
+        if (digitalRead(7) == 1) {
+          cnt--;//Decrementa contador em 1
+          PORTD = cnt;
+          delay(timer);
+        }
+      }
+    }
+  }
 }
