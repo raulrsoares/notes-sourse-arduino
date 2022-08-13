9101116470296513822813aebcbba71dac9e6b66
Raul R. Soares <83381403+raulrsoares@users.noreply.github.com>
2022-08-13 10:13:22 -0300

    Create SA3_SeteSegmentosCerto.cpp

---
+++ SA3_SeteSegmentos/SA3_SeteSegmentosCerto.cpp
@@ -0,0 +1,20 @@
+int timer = 350;
+int pino;
+int cnt;
+void setup() {
+  for (pino = 0; pino < 4; pino ++) {
+    pinMode(pino, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (cnt = 0; cnt < 10; cnt++) {
+    PORTD = cnt;
+    delay(timer);
+  }
+  for (cnt = 9; cnt > 0; cnt--) {
+    PORTD = cnt;
+    delay(timer);
+  }
+}
